797ebd54f968bb7d075cdb33c850bf78d20ce65e
rajdeepdodiya <rajdodiya216@gmail.com>
2021-07-29 03:08:16 -0400

    Developed all screens for the project and added firebase configuration.

---
+++ App.js
@@ -0,0 +1,36 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Styles from './Styles';
+import {NavigationContainer} from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack'
+import Login from './Login';
+import Signup from './Signup';
+import AddNewCache from './AddNewCache';
+import TabContainer from './TabContainer';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+    <Stack.Navigator initialRouteName="Login">
+      <Stack.Screen name="Signup" component={Signup} /> 
+      <Stack.Screen name="Login" component={Login} 
+      initialParams={{userEmail: "initialParams@initialParams.com"}}
+      options={({route}) => ({userEmail: route.params.userEmail})}
+      /> 
+      <Stack.Screen name="TabContainer" component={TabContainer}
+      initialParams={{userEmail: "rajdodiya216@gmail.com"}} 
+      options={({navigation}) => ({ headerRight : () => (
+        <Button title="+" color="#000000" onPress={() => navigation.replace("AddNewCache")}/>
+      )})
+      
+    }/>
+    <Stack.Screen name="AddNewCache" component={AddNewCache} /> 
+    </Stack.Navigator>
+  </NavigationContainer>
+  );
+}
+
+
---
+++ MapScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+
+function MapScreen(){
+
+    return(
+        <SafeAreaView>
+            <Text>Map Screen</Text>
+        </SafeAreaView>
+    );
+}
+
+export default MapScreen;
