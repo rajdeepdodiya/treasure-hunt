c0918050b741a3c5de4436bc1410052d4a5e8df1
SANDRA1ROY1 <sandraroyaricatt@gmail.com>
2021-08-03 21:11:32 -0400

    committing changes regarding list screen and details screen

--- TabContainer.js
+++ TabContainer.js
@@ -4,6 +4,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import MapScreen from './MapScreen';
 import ListScreen from './ListScreen';
+import FavouritesScreen from './FavouritesScreen';
 
 const Tab = createBottomTabNavigator();
 
@@ -19,6 +20,8 @@ const TabContainer = () => {
                 }
                 else if(route.name === "Map View"){
                     iconName = focused ? 'map' : 'map-outline';
+                }else if(route.name === "favourites"){
+                    iconName = focused ? 'heart' :'heart-outline';
                 }
                 return <Ionicons name={iconName} size={size} color={color}/>
             }})}
@@ -29,6 +32,7 @@ const TabContainer = () => {
         >
             <Tab.Screen name="List View" component={ListScreen}/>
             <Tab.Screen name="Map View" component={MapScreen}/>
+            <Tab.Screen name="favourites" component={FavouritesScreen}/>
             
         </Tab.Navigator>
     )
